a3bb5a441b2ea0975ba93d2d390841c6ecbdfb63
MaT1g3R <mat1g3r@gmail.com>
2017-12-01 02:14:29 -0500

    Pre-upload setup

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python3
 from re import match
 from pathlib import Path
+from subprocess import Popen
+from sys import argv
+
 
 from setuptools import setup, find_packages
 
 
 HERE = Path(Path(__file__).parent)
-
 META = {}
+PKGDATA = {'': ['LICENSE', 'README.md']}
+
+
+if argv[-1] == 'publish':
+    MAKE = ('python', 'setup.py', 'sdist', 'bdist_wheel')
+    UP = ('twine', 'upload', 'dist/*')
+    with Popen(MAKE) as m:
+        m.wait()
+    with Popen(UP) as u:
+        u.wait()
+    exit(0)
+
 
 with (HERE / 'pnglatex' / '__init__.py').open() as init_file:
     for line in init_file:
@@ -23,6 +38,8 @@ setup(
     entry_points={
         'console_scripts': ['pnglatex = pnglatex.pnglatex:main']
     },
+    package_data=PKGDATA,
+    include_package_data=True,
     classifiers=[
         'License :: OSI Approved :: '
         'GNU General Public License v3 or later (GPLv3+)',
