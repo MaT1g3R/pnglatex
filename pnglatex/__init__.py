0b9af3386021895122dc2910d1504d3d95abbe6e
MaT1g3R <mat1g3r@gmail.com>
2017-12-01 01:22:23 -0500

    Add __init__.py

---
+++ pnglatex/__init__.py
@@ -0,0 +1,31 @@
+"""
+pnglatex, a small program that converts latex snippets to png
+Copyright (C) 2017 Peijun Ma
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This program can be run directly as a script. It also provides the pnglatex
+function which you can import and use in your own programs.
+"""
+from .pnglatex import pnglatex
+
+__author__ = 'Peijun Ma'
+__title__ = 'pnglatex'
+__version__ = '1.0'
+__licence__ = 'GPLv3+'
+__copyright__ = 'copyright 2017 Peijun Ma'
+__url__ = 'https://github.com/MaT1g3R/pnglatex'
+__description__ = 'A small program that converts latex snippets to png'
+
+__all__ = ['pnglatex']
